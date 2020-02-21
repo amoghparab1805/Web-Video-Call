1ead7cfa7bfe21dcd7900fd8e28069a73819b502
amoghparab1805 <mumbai.amoghparab@gmail.com>
2020-02-21 20:39:48 +0530

    asdf

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
  * Config
  */
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
 
